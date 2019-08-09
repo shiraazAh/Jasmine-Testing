92d2c838c0c4bc1206236f79224eb61e72d8c025
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2019-08-09 14:58:32 +0530

    Delete a contact tesing spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -4,5 +4,12 @@ describe('Address Book', function(){
         var thisContacts = new Contact();
         addressBook.addContact(thisContacts);
         expect(addressBook.getContact(0)).toBe(thisContacts);
+    });
+    it('Should be able to delete a contact', function() {
+        var addressBook = new AddressBook();
+        var thisContacts = new Contact();
+        addressBook.addContact(thisContacts);
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).not.toBeDefined();
     })
 })
