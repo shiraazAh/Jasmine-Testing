4021d7eb0a924f7571f64e2928785f787864667a
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2019-08-09 19:57:14 +0530

    Asychronous address book , getInitial contact test code added

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,15 +1,37 @@
 describe('Address Book', function(){
+
+    var addressBook,
+        thisContacts;
+
+    beforeEach(function() {
+        addressBook = new AddressBook();
+        thisContacts = new Contact();
+    })
+
     it('Should be able to add a contact', function() {
-        var addressBook = new AddressBook();
-        var thisContacts = new Contact();
         addressBook.addContact(thisContacts);
         expect(addressBook.getContact(0)).toBe(thisContacts);
     });
+    
     it('Should be able to delete a contact', function() {
-        var addressBook = new AddressBook();
-        var thisContacts = new Contact();
         addressBook.addContact(thisContacts);
         addressBook.deleteContact(0);
         expect(addressBook.getContact(0)).not.toBeDefined();
     })
+});
+
+describe('Async Address Book', function() {
+
+    var addressBook = new AddressBook();
+
+    beforeEach(function(done){
+        addressBook.getInitialContacts(function() {
+            done();
+        });
+    });
+
+    it('Should grab initial Contacts', function(done) {
+        expect(addressBook.initialComplete).toBe(true);
+        done();
+    });
 })
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,5 +1,16 @@
 function AddressBook() {
     this.contacts = [];
+    this.initialComplete = false;
+}
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+    self = this;
+    setTimeout(function() {
+        self.initialComplete = true;
+        if (cb) {
+            return cb();
+        }
+    }, 3)
 }
 
 AddressBook.prototype.addContact = function(contact) {
