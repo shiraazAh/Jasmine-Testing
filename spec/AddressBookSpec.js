6546df3f5fb37a64b876aab4ef3211702ec63c02
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2019-08-09 14:05:55 +0530

    Workable test

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,3 +1,8 @@
 describe('Address Book', function(){
-    it('Should be able to add a contact', function)
+    it('Should be able to add a contact', function() {
+        var addressBook = new AddressBook();
+        var thisContacts = new Contact();
+        addressBook.addContact(thisContacts);
+        expect(addressBook.getContact(0)).toBe(thisContacts);
+    })
 })
